09249f4fa2438763dee0b8b67c3a2e8945821295
yichun <katrina0609@gmail.com>
2024-01-04 15:52:02 +0800

    #註冊頁畫面好了

---
+++ WebApplicationDemo/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationDemo.Models;
+
+namespace WebApplicationDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            RegisterViewModel model = new RegisterViewModel();
+            // 將模型傳遞到視圖
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            // 處理登入請求的動作
+
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            // 登出的相應動作
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            // 處理註冊請求的動作
+            return View();
+        }
+
+
+    }
+}
